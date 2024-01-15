3a16a864d823d609c305499437b101cecf6cc2e5
minyecha <hallelu1513@gmail.com>
2024-01-15 17:59:43 +0900

    이름을 입력함

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -1,9 +1,13 @@
 package project;
 
-public class Main {
+/**
+ * @author 민예찬
+ * 
+ *
+ */
 
+public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("d");
 		System.out.println("dd");
 		System.out.println("확인11");
