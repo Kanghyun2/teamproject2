95939501ea1fe04956e83e94b65a1e0a95bbbcb7
wodnr1029 <wodnr1029@nate.com>
2024-01-15 18:00:48 +0900

    이름 입력

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -1,13 +1,14 @@
 package project;
-
 /**
- * @author 민예찬
  * 
+ * @author 강재욱
  *
  */
 
 public class Main {
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println("d");
 		System.out.println("dd");
 		System.out.println("확인11");
