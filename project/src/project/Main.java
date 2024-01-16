07eaa4d213847b95fa67da1900eaa58972bd977c
wodnr1029 <wodnr1029@nate.com>
2024-01-16 10:43:52 +0900

    풀요청

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -1,21 +1,22 @@
 package project;
 /**
- * @author kanghyun, Park min, Yechan, jae
- * @author 
+ *
+ * 
+ *
+ * @author Park min, Yechan, kanghyun , jae
  * 이 클래스는 깃 기능을 연습하기 위해 작성하고 있습니다.
  * 브랜치를 새로 만들어봤어요.
  * 메인 브랜치에 최신 적용사항을 푸쉬하겠습니다.
- * 오류해결바람
- *
  */
 public class Main {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println("dd");
 		System.out.println("d");
 		System.out.println("확占쏙옙11");
 		System.out.println("d");
 		System.out.println("Ȯ��11");
-		System.out.println("풀 요청을 남겨보겠습니다.");
+		System.out.println("풀요청 재욱");
 	}
 }
