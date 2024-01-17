ecd22cb0738f22b6d78347110d33c35429b93a85
minyecha <hallelu1513@gmail.com>
2024-01-17 14:38:05 +0900

    k

--- project/src/project/GUIMain.java
+++ project/src/project/GUIMain.java
@@ -48,7 +48,7 @@ public class GUIMain extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				new Purchase();
 				dispose();
-				
+
 			}
 		});
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -66,8 +66,9 @@ public class GUIMain extends JFrame {
 				dispose();
 			}
 		});
-		
+
 	}
+
 	public void GUImathod() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 600);
@@ -77,24 +78,23 @@ public class GUIMain extends JFrame {
 		setContentPane(contentPane);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		
+
 		JPanel panel = new JPanel() {
 			@Override
-            public void paintComponent(Graphics g) {
-                g.drawImage(icon.getImage(), 0, 0, null);
-               
-                setOpaque(false); //그림을 표시하게 설정,투명하게 조절
-                super.paintComponent(g);
-            }
-        };
-        add(panel);
+			public void paintComponent(Graphics g) {
+				g.drawImage(icon.getImage(), 0, 0, null);
+
+				setOpaque(false); // 그림을 표시하게 설정,투명하게 조절
+				super.paintComponent(g);
+			}
+		};
+		add(panel);
 		contentPane.add(panel, BorderLayout.CENTER);
 		SpringLayout sl_panel = new SpringLayout();
 		panel.setLayout(sl_panel);
-		
+
 		icon = new ImageIcon("C:\\Users\\GGG\\Desktop\\테스트.jpg");
-		
-	
+
 		btnNewButton = new JButton("구매");
 		btnNewButton.setIcon(new ImageIcon("C:\\Users\\GGG\\Desktop\\버튼.png"));
 		sl_panel.putConstraint(SpringLayout.NORTH, btnNewButton, 337, SpringLayout.NORTH, panel);
@@ -103,11 +103,11 @@ public class GUIMain extends JFrame {
 		sl_panel.putConstraint(SpringLayout.EAST, btnNewButton, -655, SpringLayout.EAST, panel);
 		btnNewButton.setFont(new Font("굴림", Font.PLAIN, 30));
 		panel.add(btnNewButton);
-		
+
 		btnNewButton.setBorderPainted(false);
 		btnNewButton.setFocusPainted(false);
 		btnNewButton.setContentAreaFilled(false);
-		
+
 		btnNewButton_1 = new JButton("당첨 결과");
 		sl_panel.putConstraint(SpringLayout.NORTH, btnNewButton_1, 337, SpringLayout.NORTH, panel);
 		sl_panel.putConstraint(SpringLayout.WEST, btnNewButton_1, 57, SpringLayout.EAST, btnNewButton);
@@ -115,12 +115,10 @@ public class GUIMain extends JFrame {
 		btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\GGG\\Desktop\\버튼.png"));
 		btnNewButton_1.setFont(new Font("굴림", Font.PLAIN, 30));
 		panel.add(btnNewButton_1);
-		
+
 		btnNewButton_1.setBorderPainted(false);
 		btnNewButton_1.setFocusPainted(false);
 		btnNewButton_1.setContentAreaFilled(false);
-		
-		
 
 		btnNewButton_2 = new JButton("구매 내역");
 		sl_panel.putConstraint(SpringLayout.EAST, btnNewButton_1, -55, SpringLayout.WEST, btnNewButton_2);
@@ -131,7 +129,7 @@ public class GUIMain extends JFrame {
 		sl_panel.putConstraint(SpringLayout.EAST, btnNewButton_2, -56, SpringLayout.EAST, panel);
 		btnNewButton_2.setFont(new Font("굴림", Font.PLAIN, 30));
 		panel.add(btnNewButton_2);
-		
+
 		btnNewButton_2.setBorderPainted(false);
 		btnNewButton_2.setFocusPainted(false);
 		btnNewButton_2.setContentAreaFilled(false);
